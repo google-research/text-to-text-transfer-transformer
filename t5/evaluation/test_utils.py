ee62585ae8f1b1fae16ab63cb1b1f8975cd4c140
Adam Roberts <adarob@google.com>
2020-02-06 10:59:14 -0800

    Remove google_type_annotations.
    PiperOrigin-RevId: 293629235

--- t5/evaluation/test_utils.py
+++ t5/evaluation/test_utils.py
@@ -18,7 +18,6 @@
 
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import google_type_annotations
 from __future__ import print_function
 
 from absl.testing import absltest
